31ced31d9ef617d2a8f4a9eb7cc684d79f99824c
Suraj Mukhia <surajmukhia95@gmail.com>
2023-11-06 12:03:20 +0600

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
     web3 = new Web3(window.ethereum);
 }else{
     const provider = new Web3.providers.HttpProvider(
-        'https://rinkeby.infura.io/v3/bc5ea1e469cf4097b80cd0a25506e9ca'
+        'REPLACE WITH THE PROVIDER API'
     );
 
     web3 = new Web3(provider);
